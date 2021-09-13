008eec561b38d284047ffec1d0c0bbf74cab0cd4
shenhailong <819062373@qq.com>
2021-09-13 13:52:38 +0800

    Sag

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-19 08:41:46
+ * @LastEditTime: 2021-09-13 09:35:55
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /bom-admin/config/prod.env.js
+ */
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-   // BASE_API: '"http://39.106.96.105:8070/hups"',
-   // SINGLE_SIGN_URL: '"http://127.0.0.1:9528/#/singleSign"',// dev //本地
-	 
-   BASE_API: '"http://39.106.96.105:8070/hups"', //测试
-   SINGLE_SIGN_URL: '"http://39.106.96.105:8076/#/singleSign"' // prod
-   
+   BASE_API: '"http://39.106.96.105:8070/test"',
+  //  SINGLE_SIGN_URL: '"http://127.0.0.1:9528/#/singleSign"',// dev //本地
+
+  //  BASE_API: '"http://39.106.96.105:8070/hups"', //测试
+  //  SINGLE_SIGN_URL: '"http://39.106.96.105:8076/#/singleSign"' // prod
+
 
 }
